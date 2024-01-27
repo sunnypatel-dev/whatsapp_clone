5628b18a40bcd1d13b7e2d891242f9abc61d5c24
Ved_Patel <musicloverved37@gmail.com>
2024-01-27 16:40:09 +0530

    organizing files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
+import LandingPage from "./pages/LandingPage";
 import "./styles/main.scss";
-import Header from "./components/Header/Header";
-import LandingMain from "./components/LandingMain/LandingMain";
 
 function App() {
   return (
     <>
       <div id="app">
-        <Header />
-        <LandingMain />
+        <LandingPage />
       </div>
     </>
   );
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./LandingHeader/Header";
+import LandingMain from "./LandingMain/LandingMain";
+
+const LandingPage = () => {
+  return (
+    <>
+      <Header />
+      <LandingMain />
+    </>
+  );
+};
+
+export default LandingPage;
